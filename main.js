048e723784729ebf59bdf11091814d9acd7e3ed7
StevenG0h <atong321.as@gmail.com>
2022-03-13 13:03:05 +0700

    adding some animation

---
+++ main.js
@@ -0,0 +1,19 @@
+function navLinkActive(target) {
+    var nav = document.getElementsByClassName("nav-link");
+    for (let index = 0; index < nav.length; index++) {
+        nav[index].classList.remove("active");
+    }
+    nav[target].classList.add("active");
+    
+}
+function detectSection() {
+    var about = document.getElementById("about");
+    var event = document.getElementById("event");
+    if(event.offsetTop <= window.pageYOffset){
+        navLinkActive(2);
+    }else if(about.offsetTop <= window.pageYOffset){
+        navLinkActive(1);
+    }else{
+        navLinkActive(0);
+    }
+}
