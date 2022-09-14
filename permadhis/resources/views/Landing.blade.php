146f5804750a72f2fe07ca6d0dffd7ffec9c4919
StevenG0h <atong321.as@gmail.com>
2022-09-14 14:24:06 +0700

    revisi

--- permadhis/resources/views/Landing.blade.php
+++ permadhis/resources/views/Landing.blade.php
@@ -1,5 +1,30 @@
-@extends('landingNav.landingNav')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="shortcut icon" href="{{ asset('image/LogoPmd.png') }}" type="image/x-icon">
+    <title>Permadhis PCR</title>
+</head>
+<body onscroll="detectSection(); revealPage()">
+    <div class="nav">
+        <a href="" class="nav-home">
+            Permadhis
+        </a>
+        <div class="nav-link-wrapper">
+            <a href="#home" class="nav-link active">Beranda</a>
+            <a href="#about" class="nav-link">Tentang</a>
+            <a href="#event" class="nav-link">Event</a>
+        </div>
+    </div>
     <div class="container">
         <div class="main" id="home" style="background-image: url({{ asset('image/IMG_9616.JPG') }});">
             <div class="main-wrapper">
@@ -133,7 +158,33 @@
                 <img src="{{ asset('event/'.$data[$j]->logo[0]) }}" alt="">
                 <h2>
                     {{ $data[$j]['description'] }}
-                </h2>
+                </h2><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="shortcut icon" href="{{ asset('image/LogoPmd.png') }}" type="image/x-icon">
+    <title>Permadhis PCR</title>
+</head>
+<body onscroll="detectSection(); revealPage()">
+    <div class="nav">
+        <a href="" class="nav-home">
+            Permadhis
+        </a>
+        <div class="nav-link-wrapper">
+            <a href="#home" class="nav-link active">Beranda</a>
+            <a href="#about" class="nav-link">Tentang</a>
+            <a href="#event" class="nav-link">Event</a>
+        </div>
+    </div>
             </div>
             <div class="side-content-link">
                 <a class="side-content-link-box" href="{{ $data[$j]['instagram_link'] }}">
@@ -156,4 +207,3 @@
 </body>
 </html>
 <script src="{{ asset('js/main.js') }}"></script>
-@endSection
